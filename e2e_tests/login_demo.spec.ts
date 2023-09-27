a0d2244f1ee2503c111c57eafe4b0faebfbeba36
IraIhnatovich <i.ihnatovich@gmai.com>
2023-09-27 16:45:26 +0300

    removed some files

--- e2e_tests/login_demo.spec.ts
+++
@@ -1,9 +0,0 @@
-import {test, expect} from '@playwright/test'
-
-test ('Login test demo', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.pause();
-    await page.locator('//input[@placeholder="Username"]').fill('standard_user');
-    await page.locator('//input[@name="password"]').fill('secret_sauce');
-    await page.click('//input[@name="login-button"]');
-})
